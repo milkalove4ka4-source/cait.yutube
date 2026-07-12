ef344000d666a3a24d4241fec4c5ad11cdb677fc
milkalove4ka4-source <milkalove4ka4@gmail.com>
2026-07-12 03:10:19 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-  base: '/vibestream/', // Необходимо для корректной работы путей на GitHub Pages
+  base: '/cait.yutube/', // Исправлено под имя вашего репозитория
 })
